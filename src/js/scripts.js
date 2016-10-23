bc9abc02da77ba9785faceb9300db769aaa350df
Oskar <oskarradonkimball@gmail.com>
2016-10-23 14:55:22 -0700

    Maybe make it so header click results in random page?

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,14 +4,9 @@ $(function(){
 	$(document).on('click', 'a', function(e) {
 		e.preventDefault();
 		let href = this.getAttribute("href");
-		$.ajax({url: href});
-		if (window.location.pathname === href) {
-			return;
-		} else {
-			$('main').fadeOut(duration, function() {
-				$('main').load(href + ' main > *');
-				$('main').fadeIn(duration);
-			});
-		}	
-	});
-});
+		$('main').fadeOut(duration, function() {
+			$('main').load(href + ' main > *');
+			$('main').fadeIn(duration);
+		})
+	})
+})
