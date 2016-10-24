853762ad82a5f77fdbca1bd3e76aec56fa29ecfb
Oskar <oskarradonkimball@gmail.com>
2016-10-24 09:25:42 -0700

    Add Smoothstate, delete old jquery

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,1 @@
-let duration = 2000;
 
-$(function(){
-	$(document).on('click', 'a', function(e) {
-		e.preventDefault();
-		let href = this.getAttribute("href");
-		$('main').fadeOut(duration, function() {
-			$('main').load(href + ' main > *');
-			$('main').fadeIn(duration);
-		})
-	})
-})
