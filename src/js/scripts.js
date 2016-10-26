40db82716269c8a1e64f021cf42759cb859bcbfc
Oskar <oskarradonkimball@gmail.com>
2016-10-26 16:49:01 -0700

    push state

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,7 @@ $(function(){
 	$(document).on('click', 'a', function(e) {
 		e.preventDefault();
 		let href = this.getAttribute("href");
+    history.pushState(null, null, href);
 		$('main').fadeOut(duration, function() {
 			$('main').load(href + ' main > *');
 			$('main').fadeIn(duration + 800);
