64ecebc4d2f271ba463815a7a5607a5119d98a31
Oskar <oskarradonkimball@gmail.com>
2017-01-17 13:32:20 -0800

    Begin making text show animation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,12 @@
+document.getElementsByClassName('revealer')[0].addEventListener("click", growDiv);
 
+
+function growDiv() {
+  var growDiv = document.getElementById('grow');
+  if (growDiv.clientHeight) {
+    growDiv.style.height = 0;
+  } else {
+    var wrapper = document.querySelector('.measuringWrapper');
+    growDiv.style.height = wrapper.clientHeight + "px";
+  }
+}
