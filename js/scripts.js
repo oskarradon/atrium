8efd9600654638f952000f85444484179e9ec335
Oskar <oskarradonkimball@gmail.com>
2017-02-03 17:22:11 -0800

    Refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,8 @@
-var img = document.getElementsByClassName('revealer')[0];
-
-img.addEventListener("click", typeItOut);
-
-// Show text
-function typeItOut() {
-  console.log("hi");
-  $(function() {
+$(function() {
+  $(".revealer").click(function() {
     $(".typed").typed({
       stringsElement: $('.typed-strings'),
       showCursor: false,
     });
   });
-}
+});
